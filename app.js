a898237fd345f0eb2df00613b565818f012f4a21
Cansu Öztaş <fcansuoztas@gmail.com>
2022-09-27 14:01:21 +0300

    js

---
+++ app.js
@@ -0,0 +1,45 @@
+const apiKey= "94011cf8197348d3b6a1116fcda0abe9";
+  const url ="https://newsapi.org/v2/top-headlines?country=tr&apiKey=" +apiKey;
+
+
+const news = async ()=> {
+try{
+    const res = await fetch (url);
+    if(!res.ok){
+        hata()
+        throw new Error("İkaz")
+       
+    }
+     const data = await res.json();
+     console.log(data.articles)
+     renderNews(data.articles);
+
+}catch(err){
+    console.log(err)
+}
+}
+const renderNews = (veri)=>{
+    veri.forEach((a)=>{
+  let{author,description,url,urlToImage }= a
+document.querySelector(
+    "#news-list" ).innerHTML+=
+    `<div class="card" style="width: 18rem;">
+  <img src="${urlToImage}" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">${author}</h5>
+    <p class="card-text">${description}</p>
+    <a href="${url}" class="card-link">Devamı</a>
+  </div>`
+  
+ 
+
+    })
+}
+
+const hata =(e)=>{
+   document.querySelector("new-list") = `${e} is not found`
+}
+news()
+
+
+    
